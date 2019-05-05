f9592ddec9756ecc80761e90017d503bdab4a55d
Archita Mittal <archita810@gmail.com>
2019-05-06 03:59:53 +0530

    Added permissions

--- app/src/main/java/com/example/archi/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/archi/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.archi.myapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.btn);
+        val textureview = findViewById<TextureView>(R.id.textureview);
+
     }
 }
